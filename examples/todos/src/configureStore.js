324fb9b9b025ae5eeb21c5cd606ea064496ff639
Chengjie Lv <l8891479cj@126.com>
2017-09-17 11:29:53 +0800

    middleware chain

--- examples/todos/src/configureStore.js
+++ examples/todos/src/configureStore.js
@@ -1,43 +1,47 @@
 import { createStore } from 'redux'
 import reducer from './reducers'
 
-const addLogingToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
+const logger = (store) =>(next) => {
     if(!console.group) {
-        return rawDispatch;
+        return next;
     }
     return (action) => {
         console.group(action.type);
         console.log('%c pre state', 'color: blue', store.getState());
         console.log('%c action', 'color:green', action);
-        const returnValue = rawDispatch(action);
+        const returnValue = next(action);
         console.log('%c cur state', 'color:blue', store.getState());
         console.log('%c return value','color: gray',returnValue);
         console.groupEnd(action.type);
         return returnValue;
     }
-
 }
 
 
-const addPromiseSupport = (store) => {
-    const rawDispatch = store.dispatch;
-    return (action) => {
-        if(typeof action.then === 'function') {
-            return action.then(rawDispatch);
-        }
-        return rawDispatch(action);
-    }
 
+
+const promise = (store) => (next) =>(action) => {
+    if(typeof action.then === 'function') {
+        return action.then(next);
+    }
+    return next(action);
+}
+
+    
+const wrapMiddleware = (middlewares, store) => {
+    middlewares.slice().reverse().forEach(middleware => {
+        store.dispatch = middleware(store)(store.dispatch);
+    });
 }
 
 export const configureStore = () => {
     const store = createStore(reducer);
+    const middlewares = [promise];
     if(process.env.NODE_ENV !== 'production') {
-        store.dispatch = addLogingToDispatch(store);
+        middlewares.push(logger);
     }
 
-    store.dispatch = addPromiseSupport(store);
+    wrapMiddleware(middlewares, store);
 
     return store;
 }
